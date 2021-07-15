7743241c664ac4eee8094be5fd44fc437532365b
CyberCookie <bekon8067@gmail.com>
2021-07-15 09:28:25 +0300

    remove cjs

--- cjs/client_build/plugins/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mergePlugins = require('./merge');
-const getDefaultPlugins = require('./defaults');
-module.exports = (CONFIG, RUN_PARAMS) => mergePlugins(getDefaultPlugins(CONFIG, RUN_PARAMS), CONFIG.build.plugins);
